d4125afac189dd979ae81f4e1e7cb9956f29ba6f
Thoper15 <lexus96tr@gmail.com>
2025-10-15 15:26:04 -0400

    Fix variable declaration and image change logic

--- javascript/script-actividades.js
+++ javascript/script-actividades.js
@@ -1,15 +1,14 @@
 const buttonPrev = document.getElementById('prev')
 const buttonNext = document.getElementById('next')
-
+let x = 1
 let animacion= document.getElementById('galeria')
 function cambiarImagen() {
   animacion.style.opacity = 0
   setTimeout(() => {
-    animacion.src =`images/actividades/actividad${x}.webp`
+    animacion.src = `images/actividades/actividad${x}.webp`
     animacion.style.opacity = 1
   }, 300)
 }
-let x = 1
 buttonNext.addEventListener('click',() => {
     x==4? x=1 : x++
     cambiarImagen() 
